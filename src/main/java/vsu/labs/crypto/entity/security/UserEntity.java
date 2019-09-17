5eff0141c343b1a77a27f71571a4403c45ef5d75
potapovnr <potapovnr@gmail.com>
2019-09-17 12:24:52 +0300

    Возвращён ломбок в users

--- src/main/java/vsu/labs/crypto/entity/security/UserEntity.java
+++ src/main/java/vsu/labs/crypto/entity/security/UserEntity.java
@@ -1,5 +1,7 @@
 package vsu.labs.crypto.entity.security;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,6 +9,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
+@Data
 public class UserEntity {
 
     @Id
@@ -31,64 +34,5 @@ public class UserEntity {
     @Column(name = "role_id")
     private long roleId;
 
-    public UserEntity(Long id, String name, String surname, String login, String password, long roleId) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.login = login;
 
-        this.password = password;
-        this.roleId = roleId;
-    }
-
-    public UserEntity() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public long getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(long roleId) {
-        this.roleId = roleId;
-    }
 }

ab12bf5b7639ebddbb49ecdd2f39e8dee829580a
tosha <toxatr@icloud.com>
2019-09-18 21:57:28 +0300

    Правки после мерджа

--- src/main/java/vsu/labs/crypto/algs/encryption/elgamal/ElGamal.java
+++ src/main/java/vsu/labs/crypto/algs/encryption/elgamal/ElGamal.java
@@ -3,7 +3,8 @@ package vsu.labs.crypto.algs.encryption.elgamal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import vsu.labs.crypto.utils.math.PrimesGenerator;
-import vsu.labs.crypto.utils.math.RandomUtils;
+
+import java.math.BigInteger;
 
 public final class ElGamal {
     private static final Logger log = LoggerFactory.getLogger(ElGamal.class);
@@ -12,7 +13,7 @@ public final class ElGamal {
 
     public String encrypt(String source) {
         log.info("process method encrypt with source = {}", source);
-        Long primeP = PrimesGenerator.getPrimeLong(P_BITS);
+        BigInteger primeP = PrimesGenerator.getPrime(P_BITS);
 
         return null;
     }

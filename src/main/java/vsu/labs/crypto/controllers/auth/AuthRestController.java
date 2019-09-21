d2a253ee64582d653448f674ce34546598cd632f
tosha <toxatr@icloud.com>
2019-09-21 15:51:39 +0300

    Правки auth

--- src/main/java/vsu/labs/crypto/controllers/auth/AuthRestController.java
+++ src/main/java/vsu/labs/crypto/controllers/auth/AuthRestController.java
@@ -1,37 +1,30 @@
 package vsu.labs.crypto.controllers.auth;
 
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import vsu.labs.crypto.dto.auth.LoginRequest;
 import vsu.labs.crypto.dto.response.Response;
-import vsu.labs.crypto.entity.JpaRepository.UserRepository;
 import vsu.labs.crypto.service.auth.AuthService;
 
 
-@RestController
+@RestController @AllArgsConstructor
 @RequestMapping("auth")
 public class AuthRestController {
     private static final Logger log = LoggerFactory.getLogger(AuthRestController.class);
 
-    @Autowired
-    private AuthService authService;
-    @Autowired
-    private UserRepository userRepository;
-    @PostMapping("auth")
+    private final AuthService authService;
+
+    @PostMapping("login")
     public Response auth(@RequestBody LoginRequest request) {
         log.info("call auth");
         return Response.success(authService.authenticate(request));
     }
 
-    @PostMapping("fakeAuth")
+    @PostMapping("fake-login")
     public Response fakeAuth() {
         log.info("call fakeAuth");
         return Response.success(authService.fakeAuth());
-    }
-    @GetMapping("test")
-    public String getLogin(){
-        return userRepository.findByLogin("tosha").getLogin();
     }
 }

c10b3b4a5ec569f66a91c459a80815b3ce8a0133
tosha <toxatr@icloud.com>
2019-10-04 11:32:20 +0300

    blocks example

---
+++ src/main/java/vsu/labs/crypto/algs/common/BlocksResponse.java
@@ -0,0 +1,28 @@
+package vsu.labs.crypto.algs.common;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class BlocksResponse {
+    private List<ControlPanelBlock> blocks;
+    private Boolean withKeysGeneration = false;
+    private Boolean withStart = false;
+    private Boolean withEncrypt = false;
+
+    public static BlocksResponse withEncrypt(List<ControlPanelBlock> blocks) {
+        return new BlocksResponse(blocks, false, false, true);
+    }
+
+    public static BlocksResponse withStart(List<ControlPanelBlock> blocks) {
+        return new BlocksResponse(blocks, false, true, false);
+    }
+
+    public static BlocksResponse withEncryptAndKeys(List<ControlPanelBlock> blocks) {
+        return new BlocksResponse(blocks, true, false, true);
+    }
+}

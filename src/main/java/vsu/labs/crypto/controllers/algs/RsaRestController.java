384e758c2457bdbddca859f6458f4438e8ed53d3
potapovnr <potapovnr@gmail.com>
2019-10-21 12:33:19 +0300

    Добавлены оценки юзерам,доделан rsaController

--- src/main/java/vsu/labs/crypto/controllers/algs/RsaRestController.java
+++ src/main/java/vsu/labs/crypto/controllers/algs/RsaRestController.java
@@ -2,11 +2,15 @@ package vsu.labs.crypto.controllers.algs;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vsu.labs.crypto.algs.common.ControlPanelBlock;
 import vsu.labs.crypto.controllers.algs.abstr.AbstractAlgController;
+import vsu.labs.crypto.dto.crypto.PartitionAlgData;
 import vsu.labs.crypto.dto.response.Response;
 import vsu.labs.crypto.service.algs.sign.RsaService;
+
+import java.math.BigInteger;
+import java.util.Map;
 
 @RestController
 @RequestMapping("rsa")
@@ -20,5 +24,26 @@ public class RsaRestController extends AbstractAlgController {
         log.info("call get blocks");
         return Response.success(rsaService.getBlocks());
     }
+    @GetMapping("/generateKeys")
+    public String getKeys(){
+        return rsaService.generateKeys();
+    }
+    @PutMapping("/checkSign")
+    public String checkSign(@RequestBody Map<String, ControlPanelBlock> blocks){
+        return rsaService.checkSign(blocks);
+    }
+    @GetMapping("/stagingGenerateKeys")
+    public PartitionAlgData stagingGenerateKeys(){
+        return rsaService.stagingGenerateKeys();
+    }
+    @PostMapping("/stagingGenerateMessage")
+    public PartitionAlgData stagingGenerateMessage(@RequestBody BigInteger d,@RequestBody BigInteger n,@RequestBody String message){
+        return rsaService.stagingGenerateMessage(d,n,message);
+    }
+    @PostMapping("/stagingCheckMessage")
+    public PartitionAlgData stagingCheckMessage(@RequestBody Map<String, ControlPanelBlock> blocks){
+        return rsaService.stagingCheckMessage(blocks);
+    }
+
 
 }
--- src/main/resources/changelogs/test-data/tests/data_for_test_marks_3.4.sql
+++ src/main/resources/changelogs/test-data/tests/data_for_test_marks_3.4.sql
@@ -4,3 +4,11 @@ VALUES (5, 1, 3, 10),
       (5, 3, 6, 10),
       (5, 4, 9, 10),
       (5, 5, 1, 10);
+INSERT INTO mark(user_id, test_id, correct_answer, all_question)
+VALUES (2, 1, 7, 10),
+      (2, 2, 5, 10),
+      (2, 4, 3, 10);
+INSERT INTO mark(user_id, test_id, correct_answer, all_question)
+VALUES (6, 2, 3, 10),
+      (6, 3, 9, 10),
+      (6, 5, 6, 10);
